7cf2d3c9bc3f2a9a0cd06bbcbcd210ba10d5603e
André Kouamé <kouameandre43@gmail.com>
2025-01-22 13:53:28 +0100

    update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 const app = express();
 
 app.use('/',(req,res)=>{
-    res.send("Hello world");
+    res.send("Hello world v1");
 })
 app.listen(4000,()=>{
     console.log("server is running");
